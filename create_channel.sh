5d6e254ee097c1944058381815f118e663865db9
nodebreaker0-0 <zeroday26@gmail.com>
2021-07-07 03:11:38 +0900

    grpcurl add

--- create_channel.sh
+++ create_channel.sh
@@ -105,8 +105,20 @@ hermes -c hermes-config/config1.toml -j query client state gaia 07-tendermint-2 
 hermes -c hermes-config/config1.toml -j query connection end gaia connection-2 | jq
 hermes -c hermes-config/config1.toml -j query channel end gaia transfer channel-0 | jq
 
-for i 
+
+num=$(grpcurl -plaintext -import-path ./proto -import-path ./third_party/proto -proto ./proto/ibc/core/client/v1/query.proto localhost:9090 ibc.core.client.v1.Query/ClientStates | jq '.clientStates |  length')
+for ((i=0; i< $num; i++));
+do
+    client=$(grpcurl -plaintext -import-path ./proto -import-path ./third_party/proto -proto ./proto/ibc/core/client/v1/query.proto localhost:9090 ibc.core.client.v1.Query/ClientStates | jq -r '.clientStates | .['$i'].clientId')
+    name=$(gaiad q ibc client states -o json | jq -r '.client_states | .['$i'].client_state.chain_id')
+    connect=$(grpcurl -plaintext -import-path ./proto -import-path ./third_party/proto -proto ./proto/ibc/core/connection/v1/query.proto -d '{"client_id":"'$client'"}' localhost:9090 ibc.core.connection.v1.Query/ClientConnections | jq -r '.connectionPaths | .[0]')
+    channel=$(grpcurl -plaintext -import-path ./proto -import-path ./third_party/proto -proto ./proto/ibc/core/channel/v1/query.proto -d '{"connection":"'$connect'"}' localhost:9090 ibc.core.channel.v1.Query/ConnectionChannels | jq -r '.channels | .[].channelId')
+    echo "$name:$channel"
+done
+
+
 gaiad q ibc client states -o json | jq '.client_states | .[2].client_id' #07-tendermint-2
 gaiad q ibc client states -o json | jq '.client_states | .[2].client_state.chain_id' #terra
 gaiad q ibc connection path 07-tendermint-2 -o json | jq '.connection_paths | .[0]' #connection-1
 gaiad q ibc channel connections connection-1 -o json | jq '.channels | .[].channel_id' #channel-2      counterparty : dst chain chan
+grpcurl -plaintext -import-path ./proto -import-path ./third_party/proto -proto ./proto/ibc/core/client/v1/query.proto localhost:9090 ibc.core.client.v1.Query/ClientStates | jq '.clientStates | .[2].clientId'
